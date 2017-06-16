e6e48a3c5c20733d91a3d8f12a1172b47a65ae05
Your Name <you@example.com>
2017-06-17 01:03:12 +1200

    First Commit

---
+++ Hotdog/Hotdog/Hotdog/AzureTables.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hotdog
+{
+    public partial class AzureTables : ContentPage
+    {
+        public AzureTables()
+        {
+            InitializeComponent();
+        }
+    }
+}
